1f8b5b57d3f7929fa1e496d4c11fa9599d202a90
Paolo Ardoino <paolo.ardoino@gmail.com>
2016-04-01 16:51:00 +0100

    More code structure

--- examples/client1.js
+++ examples/client1.js
@@ -4,21 +4,21 @@ var _ = require('lodash')
 var Base = require('grenache-nodejs-base')
 var Client = require('./../lib/Client')
 
-var gc = new Base.Link({
+var link = new Base.Link({
   grape: 'ws://127.0.0.1:30001'
 })
-gc.start()
+link.start()
 
-var tc = new Client(gc, {})
-var service = tc.listen('req', 'tcp://127.0.0.1:5000')
+var client = new Client(link, {})
+var service = client.listen('req', 'tcp://127.0.0.1:5000')
 
 setInterval(function() {
-  tc.grape.announce('test', service.port, {}, () => {
+  client.grape.announce('test', service.port, {}, () => {
     console.log('announced')
   })
 }, 1000)
 
-service.socket.on('message', (rid, type, payload, reply) => {
-  //console.log('here', rid, type, payload)
-  service.socket.send([rid, type, 'world'])
+service.on('request', (rid, type, payload, handler) => {
+  console.log('HERE', rid, type, payload)
+  handler.reply('world')
 })
--- examples/client2.js
+++ examples/client2.js
@@ -4,22 +4,22 @@ var _ = require('lodash')
 var Base = require('grenache-nodejs-base')
 var Client = require('./../lib/Client')
 
-var gc = new Base.Link({
+var link = new Base.Link({
   grape: 'ws://127.0.0.1:30002'
 })
-gc.start()
+link.start()
 
-var tc = new Client(gc, {})
+var client = new Client(link, {})
 
-var cnt = 20000
+var cnt = 10
 var reps = 0
 
 setTimeout(() => {
-  gc.lookup('test', { timeout: 1000 }, (err, data) => {
+  link.lookup('test', { timeout: 1000 }, (err, data) => {
     var d1 = new Date()
     for (var i = 0; i < cnt; i++) {
-      tc.request(data[0], 'test', 'hello', (err, data) => {
-        //console.log("here", err, data)
+      client.request(data[0], 'test', 'hello', (err, data) => {
+        console.log("here", err, data)
         if (++reps === cnt) {
           var d2 = new Date()
           console.log(d2 - d1) 
